f225771fc15ec4a411dadef21a4403e015d8b6ed
Sergei Gorbunov <gorbunov@auru5.ru>
2022-10-15 18:48:07 +0600

    Update UserLocationRepository.java

--- location-service/src/main/java/com/example/locationservice/repository/UserLocationRepository.java
+++ location-service/src/main/java/com/example/locationservice/repository/UserLocationRepository.java
@@ -2,11 +2,9 @@ package com.example.locationservice.repository;
 
 import com.example.locationservice.model.UserLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.Set;
 import java.util.UUID;
 
 public interface UserLocationRepository extends JpaRepository<UserLocation, UUID>
